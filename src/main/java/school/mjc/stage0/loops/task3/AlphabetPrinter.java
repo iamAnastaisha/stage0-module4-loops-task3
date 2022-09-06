8b26e761284478b6577d935ec67289cf284faacb
nasty <anastaishastil2002@gmail.com>
2022-09-06 13:26:27 +0300

    Updated

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         for (char i = 'a'; i <= 'z'; i++) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int sum = 0;
+        t = Math.abs(t);
         for (int i = 0; i < Integer.toString(t).length(); i++) {
             sum += t % 10;
             t /= 10;
